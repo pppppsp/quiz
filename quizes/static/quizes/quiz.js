651e7dfd09f7e3f04a12f7aa1d5d0e9dfcee92a9
Gayfiev Salavat <nicecube11@ya.ru>
2023-03-07 13:00:49 +0300

    Changed

--- quizes/static/quizes/quiz.js
+++ quizes/static/quizes/quiz.js
@@ -2,6 +2,8 @@ console.log('hello world quiz')
 const url = window.location.href;
 
 const quizBox = document.getElementById('quiz-box');
+const scoreBox = document.getElementById('score-box');
+const resultBox = document.getElementById('result-box');
 
 $.ajax({
     type: 'GET',
@@ -59,8 +61,52 @@ const sendData = () => {
         type: 'POST',
         url: `${url}save/`, // ссылка на вьюху с сохранением
         data: data,
-        success: (response) => { console.log(response) },
-        error: (error) => { console.log(error) },
+        success: (response) => { 
+            const results = response.results; // присваиваем в result
+            // console.log(results)
+            quizForm.classList.add('not-visible'); // убираем форму
+
+            scoreBox.innerHTML = `${response.passed ? '<b>Прошли!</b>' : '<b>Вы не сдали!</b>'}  Ваш результат: <b>${response.score.toFixed(2)}%</b>`
+
+            results.forEach(res=>{ // прогоняем по результатам
+                const resDiv = document.createElement("div"); // 
+                for (const [question, resp] of Object.entries(res)) {
+                    // console.log(question);
+                    // console.log(resp)
+                    // console.log('***')
+
+                    resDiv.innerHTML += question // добавление вопросов в див
+                    const cls = ['container', 'p-3', 'text-light', 'h3'] // классы
+                    resDiv.classList.add(...cls) 
+
+                    if (resp == 'not answered') {  // если ответ пустой
+                        resDiv.innerHTML += '- нет ответа';
+                        resDiv.classList.add('bg-danger');
+
+                    } 
+                    else { // 
+                        const answer = resp['answered']; // ответ пользователя
+                        const correct = resp['correct_answer'] // ответ из бд
+
+                        if (answer == correct){
+                            resDiv.classList.add('bg-success');
+                            resDiv.innerHTML += ` Ваш Ответ: ${answer}`
+                        } else {
+                            resDiv.classList.add('bg-danger');
+                            resDiv.innerHTML += `| Правильный ответ: ${correct}`
+                            resDiv.innerHTML += `| Ваш ответ: ${answer}`
+                        }
+                    }
+
+                }
+                // const body = document.getElementsByTagName('BODY')[0]
+                resultBox.append(resDiv)
+
+            })
+         },
+        error: (error) => { 
+            
+         },
     })
 }
 
